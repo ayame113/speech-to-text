79e215d7750dbbf6f36f2c86cf82bf954db251ac
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-07-06 06:06:33 +0900

    use std server

--- serve.ts
+++ serve.ts
@@ -1,3 +1,4 @@
+import { serve } from "https://deno.land/std@0.193.0/http/server.ts";
 import { serveDirWithTs } from "https://deno.land/x/ts_serve@v1.4.4/mod.ts";
 
-Deno.serve((req) => serveDirWithTs(req));
+serve((req) => serveDirWithTs(req));

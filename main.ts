275da20a35e8de857be962139f2dae99e1a8fdd5
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-07-06 19:49:40 +0900

    add empty text 2

--- main.ts
+++ main.ts
@@ -20,7 +20,7 @@ recognition.interimResults = true; // 音声認識が途中でも出力する
 // 音声認識が行われた時の処理
 recognition.addEventListener("result", (e) => {
   currentOutputDiv ??= resultOutput.appendChild(document.createElement("div"));
-  currentOutputDiv.innerText ||= "…";
+  currentOutputDiv.textContent ||= "…";
   currentOutputDiv.scrollIntoView();
 
   console.log("==========");
